2673747e68b2bec891d18a85a1e7c2dd630a5a67
hilalhilalli <hilalhilalli348@gmail.com>
2022-10-10 15:04:45 +0400

    spring security without authenticationprovider , + custon login form

--- src/main/java/com/security/securitylession/config/SecurityConfig.java
+++ src/main/java/com/security/securitylession/config/SecurityConfig.java
@@ -1,28 +1,57 @@
 package com.security.securitylession.config;
 
-import com.security.securitylession.security.AuthProviderImpl;
+import com.security.securitylession.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 //@EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private AuthProviderImpl authPrrovider;
+
+
+    private PersonDetailsService personDetailsService;
 
     @Autowired
-    public void SecurityConfig(AuthProviderImpl authPrrovider) {
-        this.authPrrovider = authPrrovider;
+    SecurityConfig(PersonDetailsService personDetailsService){
+        this.personDetailsService=personDetailsService;
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable().
+                         authorizeRequests()
+                        .antMatchers("/auth/login-page","/error").permitAll()
+                        .anyRequest().authenticated()
+                        .and()
+                        .formLogin().loginPage("/auth/login-page")
+                        .loginProcessingUrl("/process_login")
+                        .defaultSuccessUrl("/home",true )
+                        .failureUrl("/auth/login-page?error");
+
     }
 
     // mes bu metod authentication edir userleri.
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authPrrovider);
+
+        // burada ariq login parolu spring security ozu yoxlayacaq.
+         auth.userDetailsService(personDetailsService);
+
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        // sifreleme use etmediyimizi bildiririk
+        // bunu passowrda {noop} yazaraqda deye bilerik.
+        return NoOpPasswordEncoder.getInstance();
     }
 }
--- src/main/java/com/security/securitylession/controller/MyController.java
+++ src/main/java/com/security/securitylession/controller/MyController.java
@@ -1,22 +1,42 @@
 package com.security.securitylession.controller;
 
 import com.security.securitylession.security.PersonDetails;
+import org.hibernate.Session;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class MyController {
     @GetMapping("/home")
-    public String getHome(){
+    public String getHome() {
+        // session RequestContextHolder-da tutulur.
+//        ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+//        HttpSession session = attr.getRequest().getSession(true); // true == allow create
+//
+//        Integer count = (Integer) session.getAttribute("count");
+//        if (count==null){
+//            count=1;
+//            session.setAttribute("count",count);
+//        }
+//
+//        System.out.println(count);
+//        session.setAttribute("count",++count);
+
         return "home";
     }
 
     @GetMapping("/show-person")
-    public String getInfoPerson(){
+    public String getInfoPerson() {
 
         // ariq PersonDetails ala bilerik
+        // PersonDetails SecurityContextHolder-da saxlanir.
+        //SecurityContextHolder bizim authenticate olmus userlerimizi saxlayir.
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
